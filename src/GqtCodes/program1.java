45820f7103d6b8fb0a68b89c03d37c68d4e55a79
yamin <yamin@192.168.100.7>
2023-04-04 06:17:22 +0530

    I love india

--- src/GqtCodes/program1.java
+++ src/GqtCodes/program1.java
@@ -5,6 +5,6 @@ public class program1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
-System.out.println("hello world");}
+System.out.println("hello GIThub");}
 
 }
